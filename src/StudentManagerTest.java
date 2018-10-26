db8bac73bac13f9706aeffea33995964c7b2332e
Nguyễn Đình Hoàng <dinhhoangvcu@gmail.com>
2018-10-26 16:21:30 +0700

    upload

---
+++ src/StudentManagerTest.java
@@ -0,0 +1,21 @@
+public class StudentManagerTest {
+    public static void main(String[] args) {
+        StudentManage studentManager = new StudentManage();
+        studentManager.displayAll();
+
+        Student andy = new Student("Andy", 23);
+        studentManager.add(andy);
+        System.out.println("added " + andy.getInfomation());
+        studentManager.displayAll();
+
+        Student booby = new Student("Booby", 25);
+        studentManager.add(booby);
+        System.out.println("added " + booby.getInfomation());
+        studentManager.displayAll();
+
+        Student samy = new Student("Samy", 22);
+        studentManager.add(samy);
+        System.out.println("added " + samy.getInfomation());
+        studentManager.displayAll();
+    }
+}
